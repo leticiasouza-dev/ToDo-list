2147f2e0d28b16ab08882662e9660536037d3465
leticiaSouzaDev <lec667121@gmail.com>
2024-11-23 17:34:51 -0300

    feat: adicionando a Header e o formulário

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,23 @@ import './global.css'
 
 import styles from './App.module.css'
 
+import { Header } from './components/Header/Header'
+
+import iconBotao from './assets/iconBotao.svg'
+
 function App() {
   return(
-    <p>olá</p>
+    <body>
+      <Header/>
+
+      <main className={styles.main}>
+          <form  className={styles.formNovaTarefa}>
+            <input type="text" placeholder='Adicione uma nova tarefa' />
+            <button type='submit'>Criar <img src={iconBotao} alt="" /> </button>
+          </form>
+      </main>
+    </body>
+    
   )
 }
 
